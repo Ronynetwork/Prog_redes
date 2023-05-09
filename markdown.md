6e7f52ed1139f9452aaeb6a41583ca22fba0ed89
Ronynetwork <122325315+Ronynetwork@users.noreply.github.com>
2023-05-08 23:44:36 -0300

    comandos básicos de socket

---
+++ markdown.md
@@ -0,0 +1,16 @@
+
+### Olá! eu sou Ronyldo Marques 👋
+<h1><h1>
+
+[![Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/ronyldo-oliveira/) [![Instagram](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/r.o.n.y.l.d.o/)
+
+![Ronyldo GitHub stats](https://github-readme-stats.vercel.app/api?username=ronynetwork&show_icons=true&theme=onedark)
+
+### **Tecnologias que utilizo**
+
+<div style="display: inline_block"><br/>
+<img align="center" alt= "html5" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"/>
+<img align="center" alt= "Python" src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white"/>
+
+<div><br/>
+Estudante da área de tecnologia com bastante expectativa no setor, visto sua importância e necessidade nos tempos atuais.
---
+++ socket_estudo.py
@@ -0,0 +1,31 @@
+# METODOS DE UM OBJETO  SOCKET
+accept()#Aceita uma conexão com o cliente
+bind(endereço)#associa um socket de servidor a um endereço
+close()#Fecha um socket, liberando todos os recursos alocados
+connect(endereço)#Conecta o cliente a um endereço
+connect_ex(endereço)#idem ao item anterior, retornando um indicador de erro em vez de uma exceção, na chamada connect de baixo nível
+getpeername()#retona ao socket local o endereço do socket remoto que está conectado
+getsockname()#Retorna o endereço do socket local
+listen()#começa a escutar pedidos de conexão
+
+# METODOS DE ENVIO E LEITURA DE BYTES
+recv()#Lê os bytes recebidos, retornando em uma string atéo tamanho limite do buffer dado pelo buffsize.
+recvfrom()#Lê os bytes recebidos, retornando em uma string atéo tamanho limite do buffer dado pelo buffsize.
+send()#Solicita o envio dos bytes pelo socket até que um conjunto de bytes seja enviado - buffer suficiente para garantir o envio.
+sendall()#envia todos os bytes passados como parâmetro, o que ocasiona sucessivos envios em camadas de sistema até que todos os bytes sejam enviados
+
+#INTERFACE DE SOCKETS
+SOCKET_STREAM()#Interface de socket de fluxo(stream), serviço com conexão confiável (TCP), dados enviadossem erros ou duplicação
+SOCKET_DGRAM()#Interface de datagrama, define uma conexão por via de (UDP), envios por pacotes independentes. Os dados podem ser perdidos
+SOCKET_RAW()#Interface de socketsbrutos. Permite acesso direto a protocolos da camada inferior, como ICMP e IP.
+
+#Criar objeto socket
+#Família:
+AF_INET()#IPv4
+AF_INET6()#IPv6
+#Socket:
+SOCKET_STREAM()#TCP
+SOCKET_DGRAM()#UDP
+
+#Protocolo
+Geralmente zero.
