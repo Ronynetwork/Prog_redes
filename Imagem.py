c1d88f638cf8bd12eeb8b1b5bedd39f238676c35
Ronynetwork <122325315+Ronynetwork@users.noreply.github.com>
2023-05-23 15:13:23 -0300

    Atualização

--- Imagem.py
+++ Imagem.py
@@ -1,4 +1,4 @@
-import socket
+import socket,sys
 
 # Solicitando o link da imagem a ser baixada:
 link = input('Insira o link da imagem em que deseja realizar o download:')
@@ -8,42 +8,54 @@ link_separado = link.split('/')
 
 # Utilizando a posição dos separadores para extrair o host:
 conexão = link_separado[0]
-print(conexão)
+
 url_host=link_separado[2]
-# apagando os valores menores que url_host:
-del link_separado[:3]
 
 # Unindo os elementos filtrados novamente em uma string:
-url_image = '/' + '/'.join(link_separado)
-
+url_image = '/'.join(link_separado)
 # Realizando a troca de extensão
 extensão = link.split('.')[-1]
-link_txt = link.replace(extensão,'txt')
-print(link_txt)
+arq_img = link.replace(extensão,'txt')
 print('-'*100);print(f'Seu host é ({url_host}).\nSua imagem é ({url_image}).');print('-'*100)
 
 # definindo o tipo de conexão:
 if conexão == 'http:':
-    url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n' 
-    host_port   = 80
-    buffer_size = 1024
-
+    try:
+        url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n' 
+        host_port   = 80
+        buffer_size = 1024
+    except:
+        print(f'Erro na conexão......: {sys.exc_info()[0]}\n')
 elif conexão == 'https:':
-    url_request = f'GET {url_image} HTTPS/1.1\r\nHOST: {url_host}\r\n\r\n' 
-    host_port   = 443
-    buffer_size = 1024
-# Buscando url da imagem:
+    try:
+        url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n' 
+        host_port   = 443
+        buffer_size = 1024
+    except:
+        print(f'Erro na conexão......: {sys.exc_info()[0]}\n')
+
 sock_img = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock_img.connect((url_host, host_port))
 sock_img.sendall(url_request.encode())
 
-print('\nBaixando a imagem...')
+print('-'*100)
+dados = sock_img.recv(buffer_size)
+dados = str(dados,'utf-8')
+print(dados)
+print('-'*100)
+
+
 # Montado a variável que armazenará os dados de retorno
 data_ret = b''
 while True:
     data = sock_img.recv(buffer_size)
     if not data: break
     data_ret += data
+
+# Criando o arquivo que armazenará os dados
+arquivo = open('dados.txt', 'w')
+arquivo.write(dados)
+arquivo.close()
 
 sock_img.close()
 
@@ -63,6 +75,6 @@ headers   = data_ret[:position]
 image     = data_ret[position+4:]
 
 # Salvando a imagem
-file_output = open('image.txt', 'wb')
+file_output = open('image.png', 'wb')
 file_output.write(image)
 file_output.close()
