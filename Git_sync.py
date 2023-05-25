61775fcbe113cfb5cc6f9cfff3fbddfa5d642848
Ronynetwork <ronyldooliveira16@gmail.com>
2023-05-25 19:21:57 -0300

    Atualizado

--- Git_sync.py
+++ Git_sync.py
@@ -3,4 +3,4 @@
 import subprocess
 subprocess.run(['git', 'add', '.'])
 subprocess.run(['git', 'commit', '-m', 'Atualizado'])
-subprocess.call(["git", "push", "-u", "origin", "master"])
+subprocess.call(["git", "push", "-u", "origin", "home"])
