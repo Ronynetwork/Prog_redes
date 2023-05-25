cf8184b0b5bc8e1056e891364ae8c57bd6f45eb8
Ronynetwork <ronyldooliveira16@gmail.com>
2023-05-25 19:21:11 -0300

    Atualizado

---
+++ Git_sync.py
@@ -0,0 +1,6 @@
+# Código para realizar commit e push automatico no GITHUB
+
+import subprocess
+subprocess.run(['git', 'add', '.'])
+subprocess.run(['git', 'commit', '-m', 'Atualizado'])
+subprocess.call(["git", "push", "-u", "origin", "master"])
