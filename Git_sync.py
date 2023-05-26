8fae13c846763c5051e0533ef132fef98114a9eb
Ronyldo Oliveira <122325315+Ronynetwork@users.noreply.github.com>
2023-05-25 21:45:49 -0300

    Delete Git_sync.py

--- Git_sync.py
+++
@@ -1,6 +0,0 @@
-# Código para realizar commit e push automatico no GITHUB
-
-import subprocess
-subprocess.run(['git', 'add', '.'])
-subprocess.run(['git', 'commit', '-m', 'Atualização'])
-subprocess.call(["git", "push", "-u", "origin", "home"])
