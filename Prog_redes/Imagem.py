e48ba4fd0b59d23cbfb8ad7de5fd71e93c210712
Ronynetwork <ronyldooliveira16@gmail.com>
2023-05-29 19:37:53 -0300

    Atualizado

--- Prog_redes/Imagem.py
+++ Prog_redes/Imagem.py
@@ -1,35 +1,30 @@
-import socket, sys, ssl, os, platform
-
-if platform.system() == 'Windows':
-    os.system('cls')
-else:
-    os.system('clear')
-
+import ssl, socket
 #url = input('informa a url: ')
-url = str('https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png')
+link = input('Insira o endereço do arquivo que você deseja baixar:')
 
-# fragmenta toda a URL
-link_quebrado = url.split('/')
+# fragmenta a URL
+link_quebrado = link.split('/')
 
 # pega apenas o host do fragmento acima
 url_host = link_quebrado[2]
 
-# pega o local da imagem
+# seleciona o local da imagem
 url_image = '/'+'/'.join(link_quebrado[3:])
 
 # pega o nome da imagem + extensão
-nome_image = link_quebrado[-1]
-print(nome_image)
-arq_image = nome_image
-# pega apenas a extensão e converte para txt
+n_img = link_quebrado[-1]
+print(n_img)
+arq_image = n_img
 extensão = arq_image.split('.')[-1]
+
+# pega apenas a extensão e converte para txt
 arq_txt = arq_image.replace(extensão, 'txt')
 
 # pega o protocolo (HTTP ou HTTPS)
-protocolo = url.split(':')[0]
-print('='*100)
-print(f"\nhostname: {url_host}\nlocal_da_imagem: {url_image}\nnome_da_imagem: {arq_image}\nextensão: {extensão}\nprotocolo: {protocolo}\n")
-print('='*100)
+protocolo = link.split(':')[0]
+print('-'*100)
+print(f"\nhost: {url_host}\n local_imagem: {url_image}\nnome_imagem: {arq_image}\nextensão: {extensão}\nprotocolo: {protocolo}\n")
+print('-'*100)
 
 # Define a porta se a url for HTTP ou HTTPS
 if protocolo == 'https':
@@ -37,8 +32,8 @@ if protocolo == 'https':
     url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n' 
 
     context = ssl.create_default_context()
-    '''   context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE'''
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
     
 
     sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,9 +49,7 @@ if protocolo == 'https':
         if not data: 
             break
         data_ret += data
-
     sock_img.close()
-
 
     # Separando o Cabeçalho dos Dados
     delimiter = '\r\n\r\n'.encode()
@@ -64,14 +57,14 @@ if protocolo == 'https':
     headers   = data_ret[:position]
     image     = data_ret[position+4:]
 
-    print('='*100,'\n')
+    print('-'*100,'\n')
     print(str(headers, 'utf-8'),'\n')
-    print('='*100)
+    print('-'*100)
 
     # Salvando a imagem
-    file_output = open(arq_image, 'wb')
+    file_output = open(n_img, 'wb')
     file_output.write(image)
     file_output.close()
 
-    with open('saida.txt', 'w', encoding='utf-8') as header:
-        header.write(headers.decode('utf-8'))
+    with open('cabeçalho.txt', 'w', encoding='utf-8') as cabeçalho:
+        cabeçalho.write(headers.decode('utf-8'))
