269df0a82f697ef942715f6f2ead231f8615451b
Ronynetwork <ronyldooliveira16@gmail.com>
2023-06-04 15:49:34 -0300

    Atualizado

--- Prog_redes/Imagem download/Imagem.py
+++ Prog_redes/Imagem download/Imagem.py
@@ -8,7 +8,6 @@ import funções_link, funções_download
 link_quebrado, url_host, url_image, n_img, arq_txt, protocolo = funções_link.link_change()
 
 print('-'*100)
-print(arq_txt)
 print(f'Host: {url_host}\nLocal_imagem: {url_image}')
 print(f'Nome_imagem: {n_img}\nProtocolo: {protocolo}')
 print('-'*100)
@@ -16,11 +15,9 @@ print('-'*100)
 # Realizando a conexão
 if protocolo == 'http':
     socket_img = funções_download.connect_http(url_host, url_image)
-    print('Conexão estabelecida!'); print('-'*100)
 
 elif protocolo == 'https':
     socket_img = funções_download.connect_https(url_host, url_image)
-    print('Conexão estabelecida!'); print('-'*100)
 
 else:
     print('Protocolo não suportado... Tente novamente com HTTP ou HTTPS\n')
@@ -42,35 +39,31 @@ position  = retorno.find(delimiter) #Pegando a posição de início do cabeçalh
 headers   = retorno[:position] # Armazenando o cabeçalho em Headers
 image     = retorno[position+4:] # Pegando apenas a imagem
 
-print('-'*100,'\n')
-print(str(headers, 'utf-8'),'\n')
-print('-'*100)
+headers = headers.decode('utf-8') # Convertendo os bytes do cabeçalho para o modelo utf-8 que precisará estar formatado mais a frente
 
 # Definindo o local do head
-nome_cabeçalho = f'\\cabeçalho.' + arq_txt
-local_cabeçalho = local + f'\\{nome_cabeçalho}'
+nome_cabeçalho = f'\\cabeçalho.' + arq_txt # Definindo o nome do  arquivo
+local_cabeçalho = local + f'\\{nome_cabeçalho}' # Definindo o diretório de download
 # salvando o head em um arquivo
 try:
-    with open(local_cabeçalho, 'w', encoding='utf-8') as header:
-        header.write(headers.decode('utf-8'))
+    with open(local_cabeçalho, 'w', encoding='utf-8') as header: # Criando o arquivo e salvando o cabeçalho no mesmo diretório
+        header.write(headers)
 except:
     print(f'Erro na criação do arquivo txt...{sys.exc_info()[0]}')
-
-headers = headers.decode('utf-8')
 try:
     linhas = headers.strip().split('\n') # pego o header já decodificado e quebro ele em linhas
     for x in linhas:
         if x.startswith('Content-Type') or x.startswith('content-type'): # Busco nessas linhas o content-type por meio do startswich que retorna True quando a palavra existir
-            extensão = x.strip().split('/')[1] # pego a linha do Content-type, retiro os espaços com strip() e quebro com split() onde tiver uma barra
-            break # com isso para o for, pois já tenho a extensão
-    html_verification = extensão.find(';') # EXCEÇÃO: quando a url é de um arquivo HTML, temos que fazer um filtro diferente para conseguir pegar a extensão
+            extensão = x.strip().split('/')[1] # localizo a linha do Content-type, retiro os espaços com strip() e quebro com split() onde tiver uma barra
+            break
+    html_verification = extensão.find(';') #Quando a url é de um arquivo HTML, temos que fazer um filtro diferente para conseguir pegar a extensão
     if html_verification != -1:
         extensão = extensão.split(';')[0] # usamos split() para quebrar a extensão onde tiver ';' e pego o primeiro resultado 
         # formato content type HTML -> html; charset = utf-8
-
 except:
     print(f'\nErro na captura do Content-Type...{sys.exc_info()[0]}\n')
 print(f'Extensão: {extensão}')
+linha_lenght = funções_link.content_lenght(headers)
 # Salvando a imagem
 nome_final = f'{n_img}.' + extensão # Definindo o nome da imagem
 imagem = local + f'\\{nome_final}' # Definindo o local de download
--- Prog_redes/Imagem download/funções_link.py
+++ Prog_redes/Imagem download/funções_link.py
@@ -30,20 +30,11 @@ def link_change():
         return link_quebrado, url_host, url_image, n_img, arq_txt, protocolo
     except:
         print(f'Erro na fragmentação... {sys.exc_info()[0]}')
-    
 
-'''def content_type (headers): # FUNÇÃO PARA RETIRAR O CONTENT-TYPE DO HEADER DE UM ARQUIVO
-    try:
-        linhas = headers.strip().split('\n') # pego o header já decodificado e quebro ele em linhas
-        for x in linhas:
-            if x.startswith('Content-Type:'): # vasculho nessas linhas o content-type por meio do startswich que retorna True quando a palavra existir
-                extensão = x.strip().split('/')[1] # pego a linha do Content-type, retiro os espaços com strip() e quebro com split() onde tiver uma barra
-                break # com isso para o for, pois já tenho a extensão
-        html_verification = extensão.find(';') # EXCEÇÃO: quando a url é de um arquivo HTML, temos que fazer um filtro diferente para conseguir pegar a extensão
-        if html_verification != -1:
-            extensão = extensão.split(';')[0] # usamos split() para quebrar a extensão onde tiver ';' e pego o primeiro resultado 
-                                              # formato content type HTML -> html; charset = utf-8
-        return extensão
-    except:
-        print(f'\nErro na captura do Content-Type...{sys.exc_info()[0]}\n')
-        '''
+def content_lenght(headers):
+    linhas = headers.strip().split('\n')  # pego o header já decodificado e quebro ele em linhas
+    for x in linhas:
+        if x.startswith('Content-Length:'): # vasculho nessas linhas o content-length por meio do startswich que retorna True quando a palavra existir
+            linha_length = int(x[16:]) # transforma em int e pega somente da posição 16 em diante
+            break
+    return linha_length 
