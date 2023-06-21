e83b22c4bb76f03394f08f1b3b8587d49360be26
Ronyldo Marques de Oliveira <20222014050006@ifrn.local>
2023-06-21 19:53:12 -0300

    Atualizado

--- cliente_servidor/cliente_udp.py
+++ cliente_servidor/cliente_udp.py
@@ -8,23 +8,37 @@ while True:
     # Solicitar o arquivo
     print('-'*100)
     nome_arquivo = input('Digite o nome do arquivo (EXIT p/ sair):');print('-'*100) # Solicitando o arquivo a ser baixado
+
     # Enviando o nome do arquivo para o servidor
     print(f'Solicitando o arquivo {nome_arquivo}');print('-'*100)
+
     udp_socket.sendto(nome_arquivo.encode(CODE_PAGE), (HOST_SERVER, SOCKET_PORT))#Enviando a  requisição do arquivo para o servidor 
     
     if nome_arquivo.upper() == 'EXIT': break #Quebrando a conexão caso o cliente digite EXIT
 
     # Recebendo o conteúdo do servidor
     dado_retorno, ip_retorno = udp_socket.recvfrom(BUFFER_SIZE) # Recebendo do servidor o dado de retorno e ip de retorno
+    dado_retorno = dado_retorno.decode(CODE_PAGE) #Deixando o arquivo legível
+
+    if 'Size:' in dado_retorno:
+        total_lenght = int(dado_retorno.split(':')[1])
 
     # Gravar o dado recebido em arquivo
-    print(f'Gravando o arquivo {nome_arquivo}');print('-'*100)
-    try:
-        with open (nome_arquivo, 'wb') as arquivo:
-            arquivo.write(dado_retorno)
-    except:
-        print(f'Erro ao salvar o arquivo...{sys.exc_info()[0]}');print('-'*100)
-        exit()
-# Fechando o socket
+    print(f'Gravando o arquivo {nome_arquivo} ({total_lenght} bytes)')
+    nome_arquivo_ = DIR_ATUAL + '\\img_client\\' + nome_arquivo
+    arquivo = open(nome_arquivo_, 'wb')
+    bytes_recebidos = 0
+    pct = 1
 
-udp_socket.close()
+    while True:
+        # Recebendo o conteúdo do servidor
+        dado_retorno, ip_retorno = udp_socket.recvfrom(BUFFER_SIZE) #Definindo a mensagem recebida e o IP do servidor que está sendo conectado
+        if not dado_retorno: break #Finalizando o laço se não possuir dados
+        print(f'Pacote ({pct}) - Dados Recebidos: {len(dado_retorno)} bytes')
+        
+        arquivo.write(dado_retorno)
+        bytes_recebidos += len(dado_retorno)
+        if bytes_recebidos >= total_lenght: break
+        pct += 1
+# Fechando o socket
+udp_socket.close()  
--- cliente_servidor/servidor_udp.py
+++ cliente_servidor/servidor_udp.py
@@ -1,4 +1,4 @@
-import socket, sys, os 
+import socket, sys, os, time
 from server_constantes import *
 
 atual_dir = os.path.dirname(os.path.abspath(__file__)) 
@@ -21,13 +21,21 @@ try:
         else:
             # Nome do arquivo a ser enviado
             nome_arquivo = atual_dir + '\\img\\' + mensagem
-            print(f'Enviando arquivo {mensagem.upper()} ')
+            print(f'Enviando arquivo {mensagem.upper()} ')  
 
-            arquivo = open(nome_arquivo, 'rb')
-            data_retorno = arquivo.read(BUFFER_SIZE)
-            udp_socket.sendto(data_retorno, ip_cliente)
+            tamanho_arquivo = os.path.getsize(nome_arquivo)
+            msg = f'Size:{tamanho_arquivo}'.encode(CODE_PAGE)
+            udp_socket.sendto(msg, ip_cliente)
 
+            arquivo = open(nome_arquivo, 'rb')# Lendo em binário a mensagem
+            while True:
+                data_retorno = arquivo.read(BUFFER_SIZE)
+                if not data_retorno: break                                
+                udp_socket.sendto(data_retorno, ip_cliente)
+                time.sleep(0.02)
+            print(f'Arquivo {mensagem.upper()} Enviado...')
             arquivo.close()
+
 except KeyboardInterrupt:
     print('Foi pressionado CTRL+C')
     # Fechando o socket
