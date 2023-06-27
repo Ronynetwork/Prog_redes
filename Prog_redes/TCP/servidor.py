92701f9aaf22caafc02210eda2eb54b14b6abe7d
Ronyldo Marques de Oliveira <20222014050006@ifrn.local>
2023-06-26 21:43:09 -0300

    Atualizado

--- Prog_redes/TCP/servidor.py
+++ Prog_redes/TCP/servidor.py
@@ -10,55 +10,49 @@ from constantes_tcp import *
 endereço = (('localhost', PORT))
 # Criando o socket UDP
 try:
+    # Abrindo a conexão TCP com o cliente
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(endereço) 
-    server.listen(1)
+    server.bind(endereço) # Associando o socket de servidor a um endereço
+    server.listen(1) # Escutando para tentar detecar uma conexão
 
     print('-'*100)
     print('Aguardando a conexão do cliente...\n')
     print('-'*100)
-except KeyboardInterrupt:
-    print(f'A conexão foi encerrada pois foi pressionado Ctrl+C')
-    server.close()
-    
-conn, end = server.accept()
-print(f'Conexão aceita!\n Cliente conectado {end}')
-# Vincular o socket a tupla (host, port)
+        
+    conn, end = server.accept() 
+    print(f'Conexão aceita!\n Cliente conectado {end}')
+    # Vincular o socket a tupla (host, port)
 
-print(f'\nSERVIDOR ATIVO: {server.getsockname()}')
-print('\nRecebendo Mensagens...\n\n')
+    print(f'\nSERVIDOR ATIVO: {server.getsockname()}')
+    print('\nRecebendo Mensagens...\n\n')
 
-try:
     while True:
-        mensagem = conn.recv(BUFFER)
-        mensagem = mensagem.decode(CODE_PAGE)
+
+        mensagem = (conn.recv(BUFFER).decode())
+
         if mensagem.upper() == 'EXIT':
             print(f'\nO {end} SE DESCONECTOU DO SERVIDOR...\n')
+            conn.close()
             server.close()
             break
-        else:
-            # Nome do arquivo a ser enviado
-            nome_arquivo = DIR_ATUAL + '\\img_server\\' + mensagem
-            print(f'Enviando arquivo {mensagem.upper()} ...')
+        
+        # Nome do arquivo a ser enviado
+        nome_arquivo = DIR_ATUAL + '\\img_server\\' + mensagem
+        print(f'Enviando arquivo {mensagem} ...')
 
-            tamanho_arquivo = os.path.getsize(nome_arquivo)
-            msg = f'Size:{tamanho_arquivo}'.encode(CODE_PAGE)
-            conn.send(msg)
+        tamanho_arquivo = os.path.getsize(nome_arquivo)
+        msg = f'Size:{tamanho_arquivo}'.encode(CODE_PAGE)
+        conn.send(msg)
 
-            arquivo = open(nome_arquivo, 'rb')
-            while True:
-                data_retorno = arquivo.read(BUFFER)
-                if not data_retorno: break                                
-                conn.send(data_retorno)
-                time.sleep(0.02)
-            print(f'Arquivo {mensagem.upper()} Enviado...')
-            arquivo.close()
-except KeyboardInterrupt:
-    print('Foi pressionado CTRL+C')
-    # Fechando o socket
-    server.close()    
+        arquivo = open(nome_arquivo, 'rb')
+        total_data_retorno = 0
+        while True:
+            data_retorno = arquivo.read(BUFFER)
+            total_data_retorno += len(data_retorno)
+            conn.send(data_retorno)
+            if not data_retorno: break                                
+            time.sleep(0.02)
+        print(f'Arquivo {mensagem} Enviado...')
+        arquivo.close()
 except:
     print(f'\nERRO: {sys.exc_info()[0]}')
-finally:    
-    # Fechando o socket
-    server.close()
