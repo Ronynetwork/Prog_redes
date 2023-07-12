b8e78ddaafd07129421d49d6fc768aad726c75ee
Ronynetwork <ronyldooliveira16@gmail.com>
2023-07-12 17:36:50 -0300

    Atualizado

---
+++ Prog_redes/PROJETO cliente_servidor/Functions_and_Var.py
@@ -0,0 +1,58 @@
+import socket, sys
+
+SERVER = '10.25.2.148'
+CLIENT = 'localhost'
+PORT = 5678
+PROMPT = 'Digite sua msg (!q para terminar) > '
+CODE = 'utf-8'
+
+def PRINTS(x):
+    print('-'*100)
+    print(x)
+    print('-'*100)
+
+def conn_server():
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('localhost', PORT))
+        sock.listen(6)
+
+        print(f'\nSERVIDOR ATIVO: {sock.getsockname()}')
+        print('\nRecebendo Mensagens...\n\n')
+        
+    except:
+        print(f'Erro ao estabaelecer a conexão do servidor{sys.exc_info()}')
+        return sock
+    
+def Client_Interaction(conn_server, end,clients):
+    command = b''
+    while command != b'!q':
+        try:
+            command = conn_server.recv(512)
+            broadCast (command, end)
+        except:
+            command = b'!q'
+            clients.remove ((conn_server, end))
+            conn_server.close()
+def Server_Interaction(sock):
+    msg = b' '
+    while msg != b'':
+        try:
+            msg = sock.recv(512)
+            print ("\n"+msg.decode(CODE)+"\n"+PROMPT)
+        except:
+            msg = b''
+    closeSocket()
+
+def closeSocket():
+    try:
+        sock.close()
+    except:
+        None
+
+def broadCast(msg, addrSource):
+    msg = f"{addrSource} -> {msg.decode('utf-8')}"
+    print (msg)
+    for sockConn, addr in client:
+        if addr != addrSource:
+            sockConn.send(msg.encode('utf-8'))
--- Prog_redes/PROJETO cliente_servidor/servidor.py
+++ Prog_redes/PROJETO cliente_servidor/servidor.py
@@ -1,8 +1,12 @@
-import threading, socket
-from constantes import *
+import threading
+from Functions_and_Var import *
 
-server, conn, end = connm_server()
+server = conn_server()
+PRINTS('\nAguardando a conexão com o cliente...\n')
+conn, end = server.accept()
+print(f'Conexão aceita!\nIp e porta do cliente conectado: |>{end[0]}, {end[1]}<|')
 try:
+    
     clients = []
     while True:  
         conn_server, end = server.accept()
