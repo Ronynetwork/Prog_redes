55610aa5465336d921996022ef16ae5bedd32746
Ronyldo Marques de Oliveira <20222014050006@ifrn.local>
2023-07-12 19:38:31 -0300

    Atualizado

--- Prog_redes/PROJETO cliente_servidor/cliente.py
+++ Prog_redes/PROJETO cliente_servidor/cliente.py
@@ -2,9 +2,11 @@ from constantes import *
 import threading
 
 try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER, PORT))
     print ("Conectado a: ", (SERVER, PORT))
     tServer = threading.Thread(target=server_interaction(sock))
-    tUser = threading.Thread(target=client_interaction())
+    tUser = threading.Thread(target=client_interaction(sock))
 
     tServer.start()
     tUser.start()
