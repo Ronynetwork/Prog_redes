a3b3da184a1fdc482e3378e78d56664e5caed76c
Ronyldo Marques de Oliveira <20222014050006@ifrn.local>
2023-07-12 20:05:38 -0300

    Atualizado

--- Prog_redes/PROJETO cliente_servidor/servidor.py
+++ Prog_redes/PROJETO cliente_servidor/servidor.py
@@ -8,7 +8,7 @@ try:
 
     while True:  
         sock_client, end = server.accept()
-        PRINTS(f'Conexão TCP estabelecida.\nCliente {end[0]} conectado na porta {end[1]}.')
+        PRINTS(f'Conexão TCP estabelecida.\n\nCliente {end[0]} conectado na porta {end[1]}.')
         clients.append((sock_client, end))
         tClient = threading.Thread(target=Client_Interaction, args=(sock_client, end))
         tClient.start()
