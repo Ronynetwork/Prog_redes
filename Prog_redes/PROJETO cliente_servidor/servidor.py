431de08b6863b9348601687da349900a82eee160
Ronyldo Marques de Oliveira <20222014050006@ifrn.local>
2023-07-12 19:45:49 -0300

    Atualizado

--- Prog_redes/PROJETO cliente_servidor/servidor.py
+++ Prog_redes/PROJETO cliente_servidor/servidor.py
@@ -1,8 +1,6 @@
 import threading, socket
 from constantes import *
 
-SERVER = '0.0.0.0'
-PORT = 7000
 
 try:
     server = conn_server()
@@ -15,6 +13,6 @@ try:
         clients.append((conn_server, end))
         tClient = threading.Thread(target=Client_Interaction, args=(sock_client, end))
         tClient.start()
-        
+
 except:
     print('Todas as portas do servidor estão ocupadas')
