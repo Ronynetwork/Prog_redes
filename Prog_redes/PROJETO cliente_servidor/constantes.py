37aa554789b3b929508901bd96c7a406a86c3bf5
Ronyldo Marques de Oliveira <20222014050006@ifrn.local>
2023-07-12 19:43:44 -0300

    Atualizado

--- Prog_redes/PROJETO cliente_servidor/constantes.py
+++ Prog_redes/PROJETO cliente_servidor/constantes.py
@@ -15,7 +15,7 @@ def conn_server():
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(('localhost', PORT))
-        PRINTS('Conexão estabelecida!')
+        PRINTS('\nServidor a espera de conexões!\n')
         server.listen(6)
 
         return server
