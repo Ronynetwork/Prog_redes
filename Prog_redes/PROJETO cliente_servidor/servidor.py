8656247ff78dbc1234a0ef836cac91b0b923a551
Ronynetwork <ronyldooliveira16@gmail.com>
2023-07-13 17:19:44 -0300

    Atualizado

--- Prog_redes/PROJETO cliente_servidor/servidor.py
+++ Prog_redes/PROJETO cliente_servidor/servidor.py
@@ -10,7 +10,7 @@ try:
         sock_client, end = server.accept()
         PRINTS(f'Conexão TCP estabelecida.\n\nCliente {end[0]} conectado na porta {end[1]}.')
         clients[end[0]] = end[1]
-        tClient = threading.Thread(target=Client_Interaction, args=(sock_client,))
+        tClient = threading.Thread(target=Client_Interaction, args=(sock_client,end,clients))
         tClient.start()
 
 except OSError as e:
