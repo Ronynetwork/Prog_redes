eca5d95a7944c939a3658ac8b07e0d20d639968a
Ronynetwork <ronyldooliveira16@gmail.com>
2023-07-14 17:37:57 -0300

    Atualizado

--- Prog_redes/PROJETO cliente_servidor/servidor.py
+++ Prog_redes/PROJETO cliente_servidor/servidor.py
@@ -5,12 +5,12 @@ from Functions_and_Var import *
 try:
     server = conn_server()
     clients = {}
-
-    sock_client, end = server.accept()
-    PRINTS(f'Conexão TCP estabelecida.\n\nCliente {end[0]} conectado na porta {end[1]}.')
-    clients[end[0]] = end[1]
-    tClient = threading.Thread(target=Client_Interaction, args=(sock_client,end,clients))
-    tClient.start()
+    while
+        sock_client, end = server.accept()
+        PRINTS(f'Conexão TCP estabelecida.\n\nCliente {end[0]} conectado na porta {end[1]}.')
+        clients[end[0]] = end[1]
+        tClient = threading.Thread(target=Client_Interaction, args=(sock_client,end,clients))
+        tClient.start()
 
 except OSError as e:
     if e.errno == 98:
