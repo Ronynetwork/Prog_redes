bf7bf83f66a3f0f523b8c8ec8cb94b9108c87652
Ronynetwork <ronyldooliveira16@gmail.com>
2023-07-14 17:43:04 -0300

    Atualizado

--- Prog_redes/PROJETO cliente_servidor/servidor.py
+++ Prog_redes/PROJETO cliente_servidor/servidor.py
@@ -6,13 +6,14 @@ try:
     server = conn_server()
     clients = {}
     while True:
-        sock_client, end = server.accept()
-        PRINTS(f'Conexão TCP estabelecida.\n\nCliente {end[0]} conectado na porta {end[1]}.')
-        clients[end[0]] = end[1]
-        tClient = threading.Thread(target=Client_Interaction, args=(sock_client,end,clients))
-        tClient.start()
+        try:
+            sock_client, end = server.accept()
+            PRINTS(f'Conexão TCP estabelecida.\n\nCliente {end[0]} conectado na porta {end[1]}.')
+            clients[end[0]] = end[1]
+            tClient = threading.Thread(target=Client_Interaction, args=(sock_client,end,clients))
+            tClient.start()
         except:
-            print()
+            print(f'Erro ao estabelecer a')
 except OSError as e:
     if e.errno == 98:
         print('Todas as portas do servidor estão ocupadas')
