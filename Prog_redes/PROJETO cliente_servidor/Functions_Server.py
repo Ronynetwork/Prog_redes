cdff6d0ab95b3879ccd36dd9a5422093a151dd73
Ronynetwork <ronyldooliveira16@gmail.com>
2023-07-14 18:10:36 -0300

    Atualizado

--- Prog_redes/PROJETO cliente_servidor/Functions_Server.py
+++ Prog_redes/PROJETO cliente_servidor/Functions_Server.py
@@ -52,16 +52,17 @@ def server_interaction(sock):
 def SPLIT(comunicacao):
     com_split = comunicacao.split()
     
-def LIST_CLIENTS(clients=None, sock=None, **kwargs):
+def List_Clients(clients=None, sock=None, **kwargs):
     try: 
         msg_title = "\nOs Clientes conectados ao Servidor são:" # formatando mensagem 
         sock.send(msg_title.encode(CODE)) 
         num = 0
-        for chave, valor in clients_dict.items():  # faço um for para pegar cada cliente conectado e enviar 
+        for chave, valor in clients.items():  # faço um for para pegar cada cliente conectado e enviar 
             ip = valor[0] # Armazenamento Temporário 
             num+=1 # formatação numeração cliente
             msg_list = f"\nCLIENTE {num}\nIP: {ip}\nPORT: {chave}\n" # formatação listagem clientes (lembrando que chave=porta e valor[0]=ip)
             sock.send(msg_list.encode(CODE)) # enviando mensagens 
     except:
         print(f'\nErro no momento de Listar os Clientes Conectados...{sys.exc_info()[0]}')  
-        exit() 
+        exit()
+def 
