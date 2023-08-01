baabb37e4fab409fa800d40f14ea0f46069069aa
Ronyldo Oliveira <122325315+Ronynetwork@users.noreply.github.com>
2023-07-31 21:03:07 -0300

    Atualização

--- PROJETO cliente_servidor/var.py
+++ PROJETO cliente_servidor/var.py
@@ -1,8 +1,7 @@
-MAX_NOT = 15
 MSG_SIZE = 512
 SERVER_CLIENT = 'localhost'
 SERVER = '0.0.0.0'
 PORT = 5678
-PROMPT = 'Digite sua mensagem ->  '
+PROMPT = 'Digite sua mensagem -> '
 CODE = 'utf-8'
 BUFFER = 4096
